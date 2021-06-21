fd91bb3b9e08c23e6445a0a5e88c5a8246b96966
Sergey Prilepko <mjcomand@mail.ru>
2021-06-21 13:56:53 +0300

    bot connected

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -1,14 +1,63 @@
 import TelegramBot from 'node-telegram-bot-api';
 import axios from 'axios';
 import { commonkeyboard } from './keyboards.js';
-import { commontext, errorcallback, letsgotosite, nocommand, youaddedtocommon } from './texts.js';
+import {
+  commontext,
+  errorcallback,
+  letsgotosite,
+  nocommand,
+  youaddedtocommon,
+} from './texts.js';
 import Circle from '../models/circle.model.js';
 import User from '../models/user.model.js';
+import arrayShuffle from 'array-shuffle';
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {
   polling: true,
 });
+
+// Фича круговорота
+
+// Circle.findOne({ name: 'common' })
+//   .populate('connected_users')
+//   .then((circle) => {
+//     const { connected_users } = circle;
+
+//     let last = null;
+//     (connected_users.length % 2)
+//       ? last = connected_users.pop()
+//       : last = null;
+    
+//     const middle = connected_users.length / 2;
+
+//     let firstArray = connected_users.slice(0, middle);
+//     let secondArray = connected_users.slice(middle, connected_users.length);
+    
+//     firstArray = arrayShuffle(firstArray);
+//     secondArray = arrayShuffle(secondArray);
+
+//     mailing(firstArray, secondArray);
+    
+//     const shuffleSecondArray = arrayShuffle(secondArray);
+
+//     mailing(shuffleSecondArray, firstArray);
+
+//     if(last) {
+//       const randomperson = shuffleSecondArray.pop();
+//       bot.sendMessage(last.chatId, `Поддержи сегодня: ${randomperson.firstName} ${randomperson.lastName}, вот его телеграм - @${randomperson.name}`);
+//       bot.sendMessage(randomperson.chatId, `Поддержи сегодня пожалуйста еще одного человека: ${last.firstName} ${last.lastName}, вот его телеграм - @${last.name}`)
+//     }
+    
+//   });
+
+// function mailing(from, to) {
+//   for (let i = 0; i < from.length; i++) {
+//     const person = from[i];
+//     const partner = to[i];
+//     bot.sendMessage(person.chatId, `Поддержи сегодня: ${partner.firstName} ${partner.lastName}, вот его телеграм - @${partner.name}`)
+//   }
+// }
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
@@ -22,45 +71,40 @@ bot.on('message', async (msg) => {
       giveMeLink(chatId);
       break;
     default:
-      bot.sendMessage(id, nocommand);
+      bot.sendMessage(chatId, nocommand);
   }
-
 });
-
 
 bot.on('callback_query', async (query) => {
   const { type, answer } = JSON.parse(query.data);
-  const { id } = query.from
-  
+  const { id } = query.from;
+
   switch (type) {
     // Ловим выбор вступать или нет в общий круговорот
-    case ('common'):
-      if(answer === 'yes') {
-        
+    case 'common':
+      if (answer === 'yes') {
         try {
-          const user = await User.findOne({chatId: id});
-          const circle = await Circle.updateOne({name: 'common'}, {$addToSet: {connected_users: user._id}}).exec()
-          if(circle.n) {
+          const user = await User.findOne({ chatId: id });
+          const circle = await Circle.updateOne(
+            { name: 'common' },
+            { $addToSet: { connected_users: user._id } }
+          ).exec();
+          if (circle.n) {
             bot.sendMessage(id, youaddedtocommon);
           }
         } catch (error) {
-          bot.sendMessage(id, "Какая-то ошибка с базой, типа " + error.message);
+          bot.sendMessage(id, 'Какая-то ошибка с базой, типа ' + error.message);
           console.log(error);
         }
-        
-        
-      } else if(answer === 'no') {
+      } else if (answer === 'no') {
         bot.sendMessage(id, letsgotosite);
       }
       break;
-    default: 
+    default:
       bot.sendMessage(id, errorcallback);
       break;
   }
 });
-
-
-
 
 async function giveMeLink(chatId) {
   const res = await linkgenerator(`${chatId}`);
@@ -94,27 +138,29 @@ async function regUser(msg) {
       lastName,
       chatId,
     })
-    .then( async (res) => {
-      await sendTimoutMessage(2000, chatId, `${person_name} вы ${res.data.message}!`)
+    .then(async (res) => {
+      await sendTimoutMessage(
+        2000,
+        chatId,
+        `${person_name} вы ${res.data.message}!`
+      );
       await sendTimoutMessage(2000, chatId, commontext, {
         reply_markup: {
-          inline_keyboard: commonkeyboard
-        }
-      })
+          inline_keyboard: commonkeyboard,
+        },
+      });
     })
     .catch((err) => {
-      bot.sendMessage(chatId, "Какая-то ошибка " + err)
-    })
+      bot.sendMessage(chatId, 'Какая-то ошибка ' + err);
+    });
 }
-
 
 function sendTimoutMessage(timeout, chatId, msg, options = {}) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      bot.sendMessage(chatId, msg, options).then(() => resolve())
+      bot.sendMessage(chatId, msg, options).then(() => resolve());
     }, timeout);
-  })
+  });
 }
-
 
 export default bot;
