dd4279362abef5fb47b826ea4ebe941a6ebe8da3
Sergey Prilepko <mjcomand@mail.ru>
2021-06-21 16:05:59 +0300

    Fixed: circles route - now circle, class - now className, imges from DB not neccessary

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import Circles from './Circles/Circles'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Circles from './Circles/Circles';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-
-
- <BrowserRouter>
-
-
-    1232332
-    <Circles/>
-
- </BrowserRouter>
-
+    <BrowserRouter>
+      <Circles />
+    </BrowserRouter>
   );
 }
 
--- client/src/Circles/Circles.js
+++ client/src/Circles/Circles.js
@@ -8,28 +8,30 @@ function Circles(props) {
   const users = useSelector((state) => state.users);
 
   useEffect(() => {
-    fetch("http://localhost:4000/circles")
+    fetch("http://localhost:4000/circle")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "INIT_CIRCLES", payload: { data } }));
   }, []);
+  
   return (
     <div>
       <div id="main">
-        <section id="banner" class="major">
-          <div class="inner">
-            <header class="major">
+        <section id="banner" className="major">
+          <div className="inner">
+            <header className="major">
               <h2>ВЫБЕРИТЕ ТЕМУ</h2>
             </header>
           </div>
         </section>
-        <section id="one" class="tiles">
+        <section id="one" className="tiles">
           {users?.map((el) => (
             <article key={el._id}>
-              <Link to={`/circule/${el._id}`}>
-                <span class="image">
-                  <img src={el.img} alt="" />
-                </span>
-                <header class="major">
+
+              <Link to={`/circle/${el._id}`}>
+                {/* <span className="image">
+                   <img src={el.img} alt="" /> 
+                </span> */}
+                <header className="major">
                   <h3>
                     {/* <Link to={`/circule/${el._id}`}>{el.name}</Link> */}
                     {el.name}
@@ -38,86 +40,9 @@ function Circles(props) {
                   <p>количество участников {0}</p>
                 </header>
               </Link>
+
             </article>
           ))}
-          {/* <article>
-            <span class="image">
-              <img src="images/pic02.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Tempus
-                </a>
-              </h3>
-              <p>feugiat amet tempus</p>
-            </header>
-          </article>
-          <article>
-            <span class="image">
-              <img src="images/pic03.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Magna
-                </a>
-              </h3>
-              <p>Lorem etiam nullam</p>
-            </header>
-          </article>
-          <article>
-            <span class="image">
-              <img src="images/pic04.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Ipsum
-                </a>
-              </h3>
-              <p>Nisl sed aliquam</p>
-            </header>
-          </article>
-          <article>
-            <span class="image">
-              <img src="images/pic05.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Consequat
-                </a>
-              </h3>
-              <p>Ipsum dolor sit amet</p>
-            </header>
-          </article>
-          <article>
-            <span class="image">
-              <img src="images/pic06.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Etiam
-                </a>
-              </h3>
-              <p>Feugiat amet tempus</p>
-            </header>
-          </article>
-          <article> */}
-          {/* <span class="image">
-              <img src="images/pic06.jpg" alt="" />
-            </span>
-            <header class="major">
-              <h3>
-                <a href="landing.html" class="link">
-                  Etiam
-                </a>
-              </h3>
-              <p>Feugiat amet tempus</p>
-            </header>
-          </article> */}
         </section>
       </div>
     </div>
