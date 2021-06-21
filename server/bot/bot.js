402996119c578ae6fd3728583d4649ac0ddf2db6
Sergey Prilepko <mjcomand@mail.ru>
2021-06-21 14:15:59 +0300

    start krugovert

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -10,54 +10,15 @@ import {
 } from './texts.js';
 import Circle from '../models/circle.model.js';
 import User from '../models/user.model.js';
-import arrayShuffle from 'array-shuffle';
+import { krugovert } from './krugovert.js';
+
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {
   polling: true,
 });
 
-// Фича круговорота
-
-// Circle.findOne({ name: 'common' })
-//   .populate('connected_users')
-//   .then((circle) => {
-//     const { connected_users } = circle;
-
-//     let last = null;
-//     (connected_users.length % 2)
-//       ? last = connected_users.pop()
-//       : last = null;
-    
-//     const middle = connected_users.length / 2;
-
-//     let firstArray = connected_users.slice(0, middle);
-//     let secondArray = connected_users.slice(middle, connected_users.length);
-    
-//     firstArray = arrayShuffle(firstArray);
-//     secondArray = arrayShuffle(secondArray);
-
-//     mailing(firstArray, secondArray);
-    
-//     const shuffleSecondArray = arrayShuffle(secondArray);
-
-//     mailing(shuffleSecondArray, firstArray);
-
-//     if(last) {
-//       const randomperson = shuffleSecondArray.pop();
-//       bot.sendMessage(last.chatId, `Поддержи сегодня: ${randomperson.firstName} ${randomperson.lastName}, вот его телеграм - @${randomperson.name}`);
-//       bot.sendMessage(randomperson.chatId, `Поддержи сегодня пожалуйста еще одного человека: ${last.firstName} ${last.lastName}, вот его телеграм - @${last.name}`)
-//     }
-    
-//   });
-
-// function mailing(from, to) {
-//   for (let i = 0; i < from.length; i++) {
-//     const person = from[i];
-//     const partner = to[i];
-//     bot.sendMessage(person.chatId, `Поддержи сегодня: ${partner.firstName} ${partner.lastName}, вот его телеграм - @${partner.name}`)
-//   }
-// }
+// krugovert(bot);
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
