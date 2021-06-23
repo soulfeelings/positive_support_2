b0cd04ac046906d61ce7aaafd40bd2ac58223df4
Sergey Prilepko <mjcomand@mail.ru>
2021-06-23 11:08:11 +0300

    После подключения/отключения в телегу летит оповещение

--- server/routes/circle.route.js
+++ server/routes/circle.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import Circle from '../models/circle.model.js';
 import User from '../models/user.model.js';
+import bot from '../bot/bot.js'
 
 const circlesRouter = Router();
 
@@ -13,28 +14,36 @@ circlesRouter.route('/')
 
 circlesRouter.route('/follow')
   .post(async (req, res) => {
+    const { currentUser } = req.body
+
     const circle = await Circle.findById(req.body.id);
-    await circle.connected_users.push(req.body.currentUser);
+    await circle.connected_users.push(currentUser);
     await circle.save();
 
-    const user = await User.findById(req.body.currentUser._id);
+    const user = await User.findById(currentUser._id);
     await user.connected_circles.push(circle);
     await user.save();
+
+    bot.sendMessage(currentUser.chatId, `Вы вступили в круговорот ${circle.name}`);
 
     res.status(200).json({circle, user})
 });
 
 circlesRouter.route('/unfollow')
   .post(async (req, res) => {
+    const { currentUser } = req.body;
+
     const circle = await Circle.findById(req.body.id);
-    const idx =  circle.connected_users.indexOf(req.body.currentUser._id);
+    const idx =  circle.connected_users.indexOf(currentUser._id);
     await circle.connected_users.splice(idx, 1)
     await circle.save();
 
-    const user = await User.findById(req.body.currentUser._id);
+    const user = await User.findById(currentUser._id);
     const index =  user.connected_circles.indexOf(req.body.id);
     await user.connected_circles.splice(index, 1)
     await user.save();
+
+    bot.sendMessage(currentUser.chatId, `Вы вышли из круговорота ${circle.name}`);
 
     res.status(200).json({circle, user})
 });
