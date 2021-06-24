509181ead3d54747111715a320044721ecd3d251
Sergey Prilepko <mjcomand@mail.ru>
2021-06-24 14:43:27 +0300

    bottransfer start

--- client/src/BotTransfer.page/BotTransferPage.js
+++ client/src/BotTransfer.page/BotTransferPage.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 // import insertStyles from '../helpers/insertStyles';
 import { botTransferPageStyles } from './BotTransferPageStyles';
-// import './BotTransfer.css';
+import classes from './BotTransfer.module.css';
+import bgimg from './images/bg.jpg';
 
 function BotTransferPage() {
 
@@ -10,25 +11,22 @@ function BotTransferPage() {
   }
 
   useEffect(() => {
-    const $style = document.createElement('style');
-    document.head.appendChild($style);
-    $style.innerHTML = botTransferPageStyles;
-    const bg = document.getElementById('bg');
-    bg.style.background = '#348cb2 url("static/media/bg.74789351.jpg") bottom left repeat-x';
-    return () => document.head.removeChild($style);
+    // const $style = document.createElement('style');
+    // document.head.appendChild($style);
+    // $style.innerHTML = botTransferPageStyles;
+    // const bg = document.getElementById('bg');
+    // bg.style.background = '#348cb2 url("static/media/bg.74789351.jpg") bottom left repeat-x';
+    // return () => document.head.removeChild($style);
   }, []);
 
   return (
-    <div className="is-preload">
-      <div id="wrapper">
-        <div id="bg"></div>
-        <div id="overlay"></div>
-        <div id="main" style={{ height: '100%' }}>
+      <div className={classes.wrapper}>
+        <div className={classes.bg} style={{backgroundImage: `url(${bgimg}) bottom left`}}></div>
+        <div className={classes.main} style={{ height: '100%' }}>
           <header id="header">
               <button onClick={() => handleClicK()}>Зарегистрируйтесь через чат бот</button>
           </header>
         </div>
-      </div>
     </div>
   );
 }
