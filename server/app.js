8834449f100b78d30ae640a68cf7712da19116c3
Kirill <k.kichkin@mail.ru>
2021-06-25 00:22:29 +0300

    build client

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+app.use(express.static(path.resolve('../client/build')))
 
 app.use('/circle', circlesRouter);
 app.use('/user', userRouter);
