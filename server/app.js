9f7fe00209c931f3da293155be91c83f93f23094
Sergey Prilepko <mjcomand@mail.ru>
2021-06-25 11:33:28 +0300

    удалили рут index

--- server/app.js
+++ server/app.js
@@ -22,9 +22,9 @@ app.use('/circle', circlesRouter);
 app.use('/user', userRouter);
 app.use('/admin', adminRouter)
 
-app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
-})
+// app.get('/', (req, res) => {
+//   res.sendFile('./public/index.html');
+// })
 
 
 export default app;
