4ba97219da1f2bc95ce7f1b4b30d0cff427b03c1
Sergey Prilepko <mjcomand@mail.ru>
2021-06-28 19:04:00 +0300

    Fixed user.route: delete unused imports

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,30 +2,10 @@
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-import Admin from '../models/admin.model.js';
+
 import User from '../models/user.model.js';
 
 const userRouter = Router();
-
-// userRouter.post('/create', async (req, res) => {
-//   try {
-//     const newUser = await User.create(req.body);
-//     await res.status(201).send({ message: 'успешно зарегистрированы' });
-//   } catch (error) {
-//     await res.status(501).send({ message: 'Ошибка регистрации' });
-//   }
-// });
-
-// userRouter.post('/login', async (req, res) => {
-//   try {
-//     const user = await User.findOneAndUpdate(
-//       { chatId: req.body.chatId },
-//       { $set: { secretId: req.body.secretId } }
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
 
 userRouter.get('/auth', authMiddleware, async (req, res) => {
   try {
